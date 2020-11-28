6fcf987c09b05cc63190ca95d747cf6501c401ac
Jamie Mills <hmills6@myune.edu.au>
2020-11-28 16:19:28 +1100

    adding packages and created some java files

---
+++ server/src/Server.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server
+
+{
+    public static void main(String[] args)
+    {
+        final int port = 8901;
+
+        // declare a general socket and a server socket
+        Socket connection;
+        ServerSocket listenSocket;
+
+        // declare low level and high level objects for input
+        InputStream inStream;
+        DataInputStream inDataStream;
+
+        // declare low level and high level objects for output
+        OutputStream outStream;
+        DataOutputStream outDataStream;
+
+        // declare other variables
+        String client;
+        int first, second, sum;
+        boolean connected;
+
+        while(true){
+
+            try{
+                // create a server socket
+                listenSocket = new ServerSocket(port);
+                System.out.println("Listening on port " + port);
+
+                // listen for a connection from the client
+                connection = listenSocket.accept();
+                connected = true;
+                System.out.println("Connection established");
+
+                // create an input stream from the client
+                inStream = connection.getInputStream(); // Returns an input stream for this socket.
+                inDataStream = new DataInputStream(inStream);
+
+                // create an output stream to the client
+                outStream = connection.getOutputStream ();
+                outDataStream = new DataOutputStream(outStream);
+
+                // wait for a string from the client
+                client = inDataStream.readUTF();
+                System.out.println("Address of client: " + client);
+
+                while(connected){
+                    // read an integer from the client
+                    first = inDataStream.readInt();
+                    System.out.println("First Number received " + first);
+
+                    // read an integer from the client
+                    second = inDataStream.readInt();
+                    System.out.println("Second number received: " + second);
+
+                    sum = first + second;
+                    System.out.println("Sum returned: " + sum);
+
+                    //send the sum to the client
+                    outDataStream.writeInt(sum);
+                }
+
+
+            } catch (IOException e) {
+                connected = false;
+            }
+        }
+
+
+    }
+
+
+}
