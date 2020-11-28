71947f2104e0ccac111c98e6cf6227d5472aa484
Jamie Mills <hmills6@myune.edu.au>
2020-11-28 17:16:53 +1100

    added server code into Controller file to communicate with Server. If not working, move to main class or creating an object file

--- client/src/ApplicationClientController.java
+++ client/src/ApplicationClientController.java
@@ -1,2 +1,64 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class ApplicationClientController {
+
+    private String remoteHost;
+    private int port;
+
+    // declare low level and high level objects for input
+    private InputStream inStream;
+    private DataInputStream inDataStream;
+
+    // declare low level and high level objects for output
+    private OutputStream outStream;
+    private DataOutputStream outDataStream;
+
+    // declare a socket
+    private Socket connection;
+
+    @FXML
+    
+
+
+    public void initialize(){
+
+        connectingWithServer();
+
+    }
+
+    public void clickToCalculateNumbers(ActionEvent event){
+
+
+    }
+
+
+    public void connectingWithServer(){
+
+        try{
+            //attempt to create a connection to the server
+            connection = new Socket(remoteHost, port);
+
+            // create an input stream from the server
+            inStream = connection.getInputStream();
+            inDataStream = new DataInputStream(inStream);
+
+            // create an output stream to the server
+            outStream = connection.getOutputStream();
+            outDataStream = new DataOutputStream(outStream);
+
+            //send the host IP to the server
+            outDataStream.writeUTF(connection.getLocalAddress().getHostAddress());
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- client/src/Client.java
+++ client/src/Client.java
@@ -4,18 +4,66 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client extends Application {
 
+//    private String remoteHost;
+//    private int port;
+//
+//    // declare low level and high level objects for input
+//    private InputStream inStream;
+//    private DataInputStream inDataStream;
+//
+//    // declare low level and high level objects for output
+//    private OutputStream outStream;
+//    private DataOutputStream outDataStream;
+//
+//    // declare a socket
+//    private Socket connection;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        loadingFXML(primaryStage);
+        //connectingWithServer();
+
+    }
+
+    public void loadingFXML(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("ApplicationClient.fxml"));
         primaryStage.setTitle("Application Client");
         primaryStage.setScene(new Scene(root, 400,400 ));
         primaryStage.show();
-
     }
+
+//    public void connectingWithServer(){
+//
+//      try{
+//          //attempt to create a connection to the server
+//          connection = new Socket(remoteHost, port);
+//
+//          // create an input stream from the server
+//          inStream = connection.getInputStream();
+//          inDataStream = new DataInputStream(inStream);
+//
+//          // create an output stream to the server
+//          outStream = connection.getOutputStream();
+//          outDataStream = new DataOutputStream(outStream);
+//
+//          //send the host IP to the server
+//          outDataStream.writeUTF(connection.getLocalAddress().getHostAddress());
+//
+//      } catch (UnknownHostException e) {
+//          e.printStackTrace();
+//      } catch (IOException e) {
+//          e.printStackTrace();
+//      }
+//
+//
+//    }
 
     public static void main(String[] args) throws IOException {
         launch(args);
