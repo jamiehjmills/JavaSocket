89b2a07699a6b3045833fd2ed482998643f00a94
Jamie Mills <hmills6@myune.edu.au>
2020-11-28 16:42:29 +1100

    creating the client window to type in its input and send it to server

---
+++ client/src/ApplicationClientController.java
@@ -0,0 +1,2 @@
+public class ApplicationClientController {
+}
--- client/src/Client.java
+++ client/src/Client.java
@@ -1,8 +1,25 @@
-public class Client {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public static void main(String[] args){
+import java.io.IOException;
 
-        System.out.println("test");
+public class Client extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ApplicationClient.fxml"));
+        primaryStage.setTitle("Application Client");
+        primaryStage.setScene(new Scene(root, 400,400 ));
+        primaryStage.show();
 
     }
+
+    public static void main(String[] args) throws IOException {
+        launch(args);
+
+    }
+
 }
